d5090c5ba591f38fd220e227e53fc87cf1ddda16
Rick Steawart <rickstewartdev@gmail.com>
2022-10-30 12:02:21 +0000

    more deployer files

--- deploy/04-setup-governor-contracts.ts
+++ deploy/04-setup-governor-contracts.ts
@@ -11,13 +11,13 @@ const setupContracts: DeployFunction = async function (
   const { getNamedAccounts, deployments } = hre;
   const { log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const timeLock = await ethers.getContract("timeLock", deployer);
+  const timeLock = await ethers.getContract("TimeLock", deployer);
   const governor = await ethers.getContract("GovernorContract", deployer);
 
   log("Setting up roles...");
   const proposerRole = await timeLock.PROPOSER_ROLE();
   const executorRole = await timeLock.EXECUTOR_ROLE();
-  const adminRole = await timeLock.ADMIN_ROLE();
+  const adminRole = await timeLock.TIMELOCK_ADMIN_ROLE();
 
   const proposerTx = await timeLock.grantRole(proposerRole, governor.address);
   await proposerTx.wait(1);
--- deploy/05-deploy-box.ts
+++ deploy/05-deploy-box.ts
@@ -8,6 +8,19 @@ const deployBox: DeployFunction = async function (
 ) {
   // @ts-ignore
   const { getNamedAccounts, deployments } = hre;
-  const { deploy, log, get } = deployments;
+  const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  log("Deploying Box...");
+  const box = await deploy("Box", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+  const boxContract = await ethers.getContract("Box", box.address);
+  const timeLock = await ethers.getContract("TimeLock");
+  const transferOwnerTx = await boxContract.transferOwnership(timeLock.address);
+  await transferOwnerTx.wait(1);
+  log("You done guud!");
 };
+
+export default deployBox;
